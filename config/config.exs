3a51101048af83ce84ae7f948c64ade3f9bdca33
Austin Ziegler <aziegler@kineticcommerce.com>
2024-04-08 23:46:00 -0400

    chore: Simplify test config

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,18 @@
 import Config
 
-import_config "#{Mix.env()}.exs"
+if Config.config_env() == :test do
+  config :logger, level: :warning
+
+  config :phoenix, :json_library, Jason
+
+  config :polymorphic_embed,
+    ecto_repos: [PolymorphicEmbed.Repo]
+
+  config :polymorphic_embed, PolymorphicEmbed.Repo,
+    username: "postgres",
+    password: "postgres",
+    database: "polymorphic_embed",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox,
+    priv: "test/support"
+end
